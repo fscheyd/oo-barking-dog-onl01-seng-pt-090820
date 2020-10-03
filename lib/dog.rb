ecde2e05bcf4a1873c055ee0673c1a8d5d657baa
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-10-03 14:41:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  def .name=
-    
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
+
+class Dog
+  def bark
+    puts "woof!"
+  end 
+  
+end 
